58765a4c00b3f1724d5a977992b3c727c1aea62e
TariShah1992 <49346783+TariShah1992@users.noreply.github.com>
2019-12-31 21:06:26 -0500

    Update RowCount.java

--- src/com/test/RowCount.java
+++ src/com/test/RowCount.java
@@ -7,9 +7,8 @@ public class RowCount {
 	public static void main(String[] args) {
 	Xls_Reader reader = new Xls_Reader("C:\\workspace\\dataDriven_firstProject\\src\\com\\data\\openSourcedata.xlsx");
 		
+		//this is for count total lines in your data sheet
 		int rowCount =reader.getRowCount("info");
-		
-		
 		System.out.println(rowCount);
 
 	}
